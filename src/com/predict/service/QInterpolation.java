b9e8253b6c980f5c262ab02659d470b2114cd7ce
ZhuChuanchuan <zhuchuanchuan0904@gmail.com>
2017-08-06 22:26:32 +0800

    大通全年流量线性插值

--- src/com/predict/service/QInterpolation.java
+++ src/com/predict/service/QInterpolation.java
@@ -1,5 +1,12 @@
 package com.predict.service;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TreeMap;
@@ -12,27 +19,66 @@ public class QInterpolation {
     }
 
     public static void main(String[] args) {
+    	
+    	//1，读入全年大通流量数据
+    	Integer[] DaTong=ReadDaTong();
+    	
+    	//2，构造线性插值函数
         Map<Date,Integer> map=new TreeMap<Date,Integer>();
         Calendar now=Calendar.getInstance();
-
-        now.set(2017,4,12,12,0,0);
-        map.put(now.getTime(),29200);
-        now.set(2017,4,13,12,0,0);
-        map.put(now.getTime(),29500);
-        now.set(2017,4,14,12,0,0);
-        map.put(now.getTime(),30200);
-        now.set(2017,4,15,12,0,0);
-        map.put(now.getTime(),30000);
-        now.set(2017,4,16,12,0,0);
-        map.put(now.getTime(),30200);
-        
+        now.set(2013,0,1,0,0,0);
+        for(int i=0;i<DaTong.length;i++){
+    		//System.out.println(DaTong[i]);
+            map.put(now.getTime(),DaTong[i]);
+            now.add(Calendar.DAY_OF_YEAR, 1);
+            
+    	}
+          
         Calendar instance=Calendar.getInstance();
         instance.set(2010,0,1,0,0,0);
         
-        Calendar now2=Calendar.getInstance();
-        now2.set(2017,4,13,12,8,0);
-        System.out.println(qInterpolation(now2.getTime(),instance.getTime(),map));
         
+        BufferedWriter writer=null;
+        try {
+        	File file=new File("大通2013插值后.txt");
+            if(file.exists()){
+            	file.delete();
+            }
+			file.createNewFile();
+			writer=new BufferedWriter(new FileWriter(file));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        Calendar start=Calendar.getInstance();
+        start.set(2013,0,1,0,0,0);
+        Calendar end=Calendar.getInstance();
+        end.set(2013,11,31,0,0,0);
+        
+        int count=0;
+        //3，获取插值，写入文件
+        try {
+    		while(start.compareTo(end)<0){
+    			double datong=qInterpolation(start.getTime(),instance.getTime(),map)/10000;
+    			//System.out.println(start.getTime()+" "+String.valueOf(datong));
+    			writer.write(String.valueOf(datong));
+        		writer.write("\n");
+            	
+            	start.add(Calendar.MINUTE, 30);
+            	count++;
+            }
+    		//System.out.println(start.getTime()+" "+datong);
+			writer.close();
+			System.out.println("共有插值后总数"+count);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
+        
+//        System.out.println(qInterpolation(now2.getTime(),instance.getTime(),map));
+//        
         /*
         Date dt=new Date();
         Calendar c=Calendar.getInstance();
@@ -43,7 +89,33 @@ public class QInterpolation {
 		 System.out.println(now.getTime());
 */
     }
-    private static double getHours(Date date1,Date date2){
+    private static Integer[] ReadDaTong() {
+    	FileReader preResult_fr;
+		BufferedReader preResult_br;
+		String preResultPath = "DaTong2013.txt";
+		int count = 0;
+		Integer[] DaTong=new Integer[365];
+		try {
+			preResult_fr = new FileReader(preResultPath);
+			preResult_br = new BufferedReader(preResult_fr);
+			String strLine = "";
+			try {
+				while ((strLine = preResult_br.readLine()) != null) {
+					DaTong[count++]=Integer.parseInt(strLine);
+				}
+				System.out.println("共大通天数条数："+count);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return DaTong;
+	}
+
+	private static double getHours(Date date1,Date date2){
         long time=date1.getTime()-date2.getTime();
         double hours=(time/1000)/3600.0;
         return hours;
